87556ac947bf0decd2ca6ce30d66fd69c065a0cf
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-01-17 17:19:06 -0300

    feat: adiciona função responsável por contar letras, palavras e sentenças

--- main.c
+++ main.c
@@ -2,12 +2,37 @@
 #include <locale.h>
 #include <stdlib.h>
 
+int counter (char text[]);
+
 int main(int argc, char const *argv[])
 {
   setlocale(LC_ALL, "Portuguese_Brazil");
 
-  printf("Oi");
+  char text[1000];
+
+  printf("Texto: ");
+  fgets(text, sizeof(text), stdin);
 
 
   return 0;
+}
+
+int counter (char text[]) {
+  int letters = 0;
+  int words = 1;
+  int sentences = 0;
+
+  for (int i = 0; text[i] != '\0'; i++) {
+    int character = text[i];
+
+    if ( (character >= 65 && character <= 90) || (character >= 97 && character <= 122) ) {
+      letters++;
+    }
+    else if ( character == 32 ) {
+      words++;
+    }
+    else if ( character == 33 || character == 43 || character == 63 ) {}
+  }
+
+  return letters, words, sentences;
 }
